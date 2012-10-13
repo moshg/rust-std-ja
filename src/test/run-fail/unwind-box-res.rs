catamorphism/rust

Make moves explicit in rfail tests

src/test/run-fail/unwind-box-res.rs
@@ -20,7 +20,7 @@ fn r(v: *int) -> r {
 fn main() unsafe {
     let i1 = ~0;
     let i1p = unsafe::reinterpret_cast(&i1);
-    unsafe::forget(i1);
+    unsafe::forget(move i1);
     let x = @r(i1p);
     failfn();
     log(error, x);

moshg/rust-std-ja

src/test/run-fail/unwind-box-res.rs
@@ -20,7 +20,7 @@ fn r(v: *int) -> r {
 fn main() unsafe {
     let i1 = ~0;
     let i1p = cast::reinterpret_cast(&i1);
-    cast::forget(i1);
+    cast::forget(move i1);
     let x = @r(i1p);
     failfn();
     log(error, x);