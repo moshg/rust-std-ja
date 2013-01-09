moshg/rust-std-ja

commit a2b9d76f21cccc97c5532b7c0d283c24c909dfa8

librustc: Make unqualified identifier searches terminate at the nearest module scope. r=tjc

src/compiletest/procsrv.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use core::prelude::*;
+
 use io;
 use io::{ReaderUtil, WriterUtil};
 use libc;

commit 85b270f5011264c5e34c1b5436e77c48d598da85

compiletest: more win32 fallout from a2b9d76

src/compiletest/procsrv.rs
@@ -20,6 +20,7 @@ use run::spawn_process;
 use pipes;
 use str;
 use task;
+use vec;
 
 export run;
 