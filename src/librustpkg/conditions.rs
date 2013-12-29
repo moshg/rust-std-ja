luisbg/rust

Rename uses of PkgId to CrateId in librustpkg

src/librustpkg/conditions.rs
@@ -10,7 +10,7 @@
 
 // Useful conditions
 
-pub use package_id::PkgId;
+pub use package_id::CrateId;
 pub use std::io::FileStat;
 pub use std::io::process::ProcessExit;
 pub use std::path::Path;
@@ -20,15 +20,15 @@ condition! {
 }
 
 condition! {
-    pub nonexistent_package: (PkgId, ~str) -> Path;
+    pub nonexistent_package: (CrateId, ~str) -> Path;
 }
 
 condition! {
-    pub missing_pkg_files: (PkgId) -> ();
+    pub missing_pkg_files: (CrateId) -> ();
 }
 
 condition! {
-    pub bad_pkg_id: (Path, ~str) -> PkgId;
+    pub bad_pkg_id: (Path, ~str) -> CrateId;
 }
 
 condition! {

src/librustpkg/workspace.rs
@@ -16,20 +16,20 @@ use context::Context;
 use path_util::{workspace_contains_package_id, find_dir_using_rust_path_hack, default_workspace};
 use path_util::rust_path;
 use util::option_to_vec;
-use package_id::PkgId;
+use package_id::CrateId;
 
 pub fn each_pkg_parent_workspace(cx: &Context,
-                                 pkgid: &PkgId,
+                                 crateid: &CrateId,
                                  action: |&Path| -> bool)
                                  -> bool {
     // Using the RUST_PATH, find workspaces that contain
     // this package ID
-    let workspaces = pkg_parent_workspaces(cx, pkgid);
+    let workspaces = pkg_parent_workspaces(cx, crateid);
     if workspaces.is_empty() {
         // tjc: make this a condition
         fail!("Package {} not found in any of \
                     the following workspaces: {}",
-                   pkgid.path.display(),
+                   crateid.path.display(),
                    rust_path().map(|p| p.display().to_str()).to_str());
     }
     for ws in workspaces.iter() {
@@ -42,12 +42,12 @@ pub fn each_pkg_parent_workspace(cx: &Context,
 
 /// Given a package ID, return a vector of all of the workspaces in
 /// the RUST_PATH that contain it
-pub fn pkg_parent_workspaces(cx: &Context, pkgid: &PkgId) -> ~[Path] {
+pub fn pkg_parent_workspaces(cx: &Context, crateid: &CrateId) -> ~[Path] {
     let rs: ~[Path] = rust_path().move_iter()
-        .filter(|ws| workspace_contains_package_id(pkgid, ws))
+        .filter(|ws| workspace_contains_package_id(crateid, ws))
         .collect();
     if cx.use_rust_path_hack {
-        rs + option_to_vec(find_dir_using_rust_path_hack(pkgid))
+        rs + option_to_vec(find_dir_using_rust_path_hack(crateid))
     }
     else {
         rs
@@ -56,7 +56,7 @@ pub fn pkg_parent_workspaces(cx: &Context, pkgid: &PkgId) -> ~[Path] {
 
 /// Construct a workspace and package-ID name based on the current directory.
 /// This gets used when rustpkg gets invoked without a package-ID argument.
-pub fn cwd_to_workspace() -> Option<(Path, PkgId)> {
+pub fn cwd_to_workspace() -> Option<(Path, CrateId)> {
     let cwd = os::getcwd();
     for path in rust_path().move_iter() {
         let srcpath = path.join("src");
@@ -64,7 +64,7 @@ pub fn cwd_to_workspace() -> Option<(Path, PkgId)> {
             let rel = cwd.path_relative_from(&srcpath);
             let rel_s = rel.as_ref().and_then(|p|p.as_str());
             if rel_s.is_some() {
-                return Some((path, PkgId::new(rel_s.unwrap())));
+                return Some((path, CrateId::new(rel_s.unwrap())));
             }
         }
     }

moshg/rust-std-ja

src/librustpkg/conditions.rs
@@ -10,7 +10,7 @@
 
 // Useful conditions
 
-pub use package_id::PkgId;
+pub use crate_id::CrateId;
 pub use std::io::FileStat;
 pub use std::io::process::ProcessExit;
 pub use std::path::Path;
@@ -20,15 +20,15 @@ condition! {
 }
 
 condition! {
-    pub nonexistent_package: (PkgId, ~str) -> Path;
+    pub nonexistent_package: (CrateId, ~str) -> Path;
 }
 
 condition! {
-    pub missing_pkg_files: (PkgId) -> ();
+    pub missing_pkg_files: (CrateId) -> ();
 }
 
 condition! {
-    pub bad_pkg_id: (Path, ~str) -> PkgId;
+    pub bad_pkg_id: (Path, ~str) -> CrateId;
 }
 
 condition! {

src/librustpkg/workspace.rs
@@ -13,23 +13,23 @@
 use std::os;
 use std::path::Path;
 use context::Context;
-use path_util::{workspace_contains_package_id, find_dir_using_rust_path_hack, default_workspace};
+use path_util::{workspace_contains_crate_id, find_dir_using_rust_path_hack, default_workspace};
 use path_util::rust_path;
 use util::option_to_vec;
-use package_id::PkgId;
+use crate_id::CrateId;
 
 pub fn each_pkg_parent_workspace(cx: &Context,
-                                 pkgid: &PkgId,
+                                 crateid: &CrateId,
                                  action: |&Path| -> bool)
                                  -> bool {
     // Using the RUST_PATH, find workspaces that contain
     // this package ID
-    let workspaces = pkg_parent_workspaces(cx, pkgid);
+    let workspaces = pkg_parent_workspaces(cx, crateid);
     if workspaces.is_empty() {
         // tjc: make this a condition
         fail!("Package {} not found in any of \
                     the following workspaces: {}",
-                   pkgid.path.display(),
+                   crateid.path.display(),
                    rust_path().map(|p| p.display().to_str()).to_str());
     }
     for ws in workspaces.iter() {
@@ -42,12 +42,12 @@ pub fn each_pkg_parent_workspace(cx: &Context,
 
 /// Given a package ID, return a vector of all of the workspaces in
 /// the RUST_PATH that contain it
-pub fn pkg_parent_workspaces(cx: &Context, pkgid: &PkgId) -> ~[Path] {
+pub fn pkg_parent_workspaces(cx: &Context, crateid: &CrateId) -> ~[Path] {
     let rs: ~[Path] = rust_path().move_iter()
-        .filter(|ws| workspace_contains_package_id(pkgid, ws))
+        .filter(|ws| workspace_contains_crate_id(crateid, ws))
         .collect();
     if cx.use_rust_path_hack {
-        rs + option_to_vec(find_dir_using_rust_path_hack(pkgid))
+        rs + option_to_vec(find_dir_using_rust_path_hack(crateid))
     }
     else {
         rs
@@ -56,7 +56,7 @@ pub fn pkg_parent_workspaces(cx: &Context, pkgid: &PkgId) -> ~[Path] {
 
 /// Construct a workspace and package-ID name based on the current directory.
 /// This gets used when rustpkg gets invoked without a package-ID argument.
-pub fn cwd_to_workspace() -> Option<(Path, PkgId)> {
+pub fn cwd_to_workspace() -> Option<(Path, CrateId)> {
     let cwd = os::getcwd();
     for path in rust_path().move_iter() {
         let srcpath = path.join("src");
@@ -64,7 +64,7 @@ pub fn cwd_to_workspace() -> Option<(Path, PkgId)> {
             let rel = cwd.path_relative_from(&srcpath);
             let rel_s = rel.as_ref().and_then(|p|p.as_str());
             if rel_s.is_some() {
-                return Some((path, PkgId::new(rel_s.unwrap())));
+                return Some((path, CrateId::new(rel_s.unwrap())));
             }
         }
     }