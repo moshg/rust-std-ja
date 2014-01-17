moshg/rust-std-ja

commit 0c77eb21e5965419c98a6ac0de7cd9a5ca8fef39

doc: tidy up makefile rules

mk/docs.mk
@@ -15,6 +15,10 @@
 DOCS :=
 DOCS_L10N :=
 
+BASE_DOC_OPTS := --from=markdown --standalone --toc --number-sections --include-before-body=doc/version_info.html
+HTML_OPTS = $(BASE_DOC_OPTS) --to=html5  --section-divs --css=rust.css --include-in-header=doc/favicon.inc
+TEX_OPTS = $(BASE_DOC_OPTS) --to=latex
+EPUB_OPTS = $(BASE_DOC_OPTS) --to=epub
 
 ######################################################################
 # Docs, from pandoc, rustdoc (which runs pandoc), and node
@@ -49,86 +53,45 @@ doc/rust.html: rust.md doc/version_info.html doc/rust.css doc/manual.inc \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-	"$(CFG_PANDOC)" \
-         --standalone --toc \
-         --section-divs \
-         --number-sections \
-         --from=markdown --to=html5 \
-         --css=rust.css --include-in-header=doc/manual.inc \
-         --include-in-header=doc/favicon.inc \
-         --include-before-body=doc/version_info.html \
-         --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --include-in-header=doc/manual.inc --output=$@
 
 DOCS += doc/rust.tex
 doc/rust.tex: rust.md doc/version.md
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js $< | \
-	"$(CFG_PANDOC)" \
-         --standalone --toc \
-         --number-sections \
-         --include-before-body=doc/version.md \
-         --from=markdown --to=latex \
-         --output=$@
+	$(CFG_PANDOC) $(TEX_OPTS) --output=$@
 
 DOCS += doc/rust.epub
-doc/rust.epub: rust.md doc/version_info.html doc/rust.css doc/manual.inc
+doc/rust.epub: rust.md doc/version_info.html doc/rust.css
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-	"$(CFG_PANDOC)" \
-         --standalone --toc \
-         --section-divs \
-         --number-sections \
-         --from=markdown --to=epub \
-         --include-before-body=doc/version_info.html \
-         --output=$@
+	$(CFG_PANDOC) $(EPUB_OPTS) --output=$@
 
 DOCS += doc/rustpkg.html
-doc/rustpkg.html: rustpkg.md doc/version_info.html doc/rust.css doc/manual.inc \
+doc/rustpkg.html: rustpkg.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-	"$(CFG_PANDOC)" \
-         --standalone --toc \
-         --section-divs \
-         --number-sections \
-         --from=markdown --to=html5 \
-         --css=rust.css --include-in-header=doc/manual.inc \
-         --include-in-header=doc/favicon.inc \
-         --include-before-body=doc/version_info.html \
-         --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial.html
 doc/tutorial.html: tutorial.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial.tex
 doc/tutorial.tex: tutorial.md doc/version.md
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js $< | \
-	"$(CFG_PANDOC)" \
-         --standalone --toc \
-         --number-sections \
-         --include-before-body=doc/version.md \
-         --from=markdown --to=latex \
-         --output=$@
+	$(CFG_PANDOC) $(TEX_OPTS) --output=$@
 
 DOCS += doc/tutorial.epub
 doc/tutorial.epub: tutorial.md doc/version_info.html doc/rust.css
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=epub \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(EPUB_OPTS) --output=$@
 
 
 DOCS_L10N += doc/l10n/ja/tutorial.html
@@ -146,84 +109,49 @@ doc/tutorial-macros.html: tutorial-macros.md doc/version_info.html doc/rust.css 
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial-container.html
 doc/tutorial-container.html: tutorial-container.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial-ffi.html
 doc/tutorial-ffi.html: tutorial-ffi.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial-borrowed-ptr.html
 doc/tutorial-borrowed-ptr.html: tutorial-borrowed-ptr.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial-tasks.html
 doc/tutorial-tasks.html: tutorial-tasks.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial-conditions.html
 doc/tutorial-conditions.html: tutorial-conditions.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
 DOCS += doc/tutorial-rustpkg.html
 doc/tutorial-rustpkg.html: tutorial-rustpkg.md doc/version_info.html doc/rust.css \
 				doc/favicon.inc
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
-          $(CFG_PANDOC) --standalone --toc \
-           --section-divs --number-sections \
-           --from=markdown --to=html5 --css=rust.css \
-           --include-in-header=doc/favicon.inc \
-           --include-before-body=doc/version_info.html \
-           --output=$@
+	$(CFG_PANDOC) $(HTML_OPTS) --output=$@
 
   ifeq ($(CFG_PDFLATEX),)
     $(info cfg: no pdflatex found, omitting doc/rust.pdf)

commit 0e71269fe741c48b03b90ab900c607c8ef815ba7

doc: fix version stamp for TeX files
This fixes a regression introduced in
0c77eb21e5965419c98a6ac0de7cd9a5ca8fef39.

mk/docs.mk
@@ -17,16 +17,16 @@ CDOCS :=
 DOCS_L10N :=
 HTML_DEPS :=
 
-BASE_DOC_OPTS := --include-before-body=doc/version_info.html --standalone  \
-                                --toc --number-sections
-HTML_OPTS = $(BASE_DOC_OPTS) 	--to=html5  --section-divs --css=rust.css  \
-								--include-in-header=doc/favicon.inc
-TEX_OPTS = $(BASE_DOC_OPTS) --to=latex
+BASE_DOC_OPTS := --standalone --toc --number-sections
+HTML_OPTS = $(BASE_DOC_OPTS) --to=html5 --section-divs --css=rust.css \
+    --include-before-body=doc/version_info.html --include-in-header=doc/favicon.inc
+TEX_OPTS = $(BASE_DOC_OPTS) --include-before-body=doc/version.md --to=latex
 EPUB_OPTS = $(BASE_DOC_OPTS) --to=epub
 
 ######################################################################
 # Rust version
 ######################################################################
+
 doc/version.md: $(MKFILE_DEPS) $(wildcard $(S)doc/*.*)
 	@$(call E, version-stamp: $@)
 	$(Q)echo "$(CFG_VERSION)" >$@
@@ -84,7 +84,7 @@ doc/rust.tex: rust.md doc/version.md
 	$(CFG_PANDOC) $(TEX_OPTS) --output=$@
 
 DOCS += doc/rust.epub
-doc/rust.epub: rust.md doc/version_info.html doc/rust.css
+doc/rust.epub: rust.md
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
 	$(CFG_PANDOC) $(EPUB_OPTS) --output=$@
@@ -114,7 +114,7 @@ doc/tutorial.tex: tutorial.md doc/version.md
 	$(CFG_PANDOC) $(TEX_OPTS) --output=$@
 
 DOCS += doc/tutorial.epub
-doc/tutorial.epub: tutorial.md doc/version_info.html doc/rust.css
+doc/tutorial.epub: tutorial.md
 	@$(call E, pandoc: $@)
 	$(Q)$(CFG_NODE) $(S)doc/prep.js --highlight $< | \
 	$(CFG_PANDOC) $(EPUB_OPTS) --output=$@
@@ -265,6 +265,7 @@ endif # No pandoc / node
 ######################################################################
 # LLnextgen (grammar analysis from refman)
 ######################################################################
+
 ifeq ($(CFG_LLNEXTGEN),)
   $(info cfg: no llnextgen found, omitting grammar-verification)
 else