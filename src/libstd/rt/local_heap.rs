rust-lang/rust

libstd: Add some methods to `Vec<T>`.

src/libstd/rt/local_heap.rs
@@ -21,7 +21,7 @@ use rt::global_heap;
 use rt::local::Local;
 use rt::task::Task;
 use raw;
-use vec::ImmutableVector;
+use vec::{ImmutableVector, Vector};
 use vec_ng::Vec;
 
 // This has no meaning with out rtdebug also turned on.

src/libstd/str.rs
@@ -101,6 +101,7 @@ use ptr::RawPtr;
 use from_str::FromStr;
 use vec;
 use vec::{OwnedVector, OwnedCloneableVector, ImmutableVector, MutableVector};
+use vec::{Vector};
 use vec_ng::Vec;
 use default::Default;
 use raw::Repr;

moshg/rust-std-ja

src/libstd/rt/local_heap.rs
@@ -21,7 +21,7 @@ use rt::global_heap;
 use rt::local::Local;
 use rt::task::Task;
 use raw;
-use slice::ImmutableVector;
+use slice::{ImmutableVector, Vector};
 use vec::Vec;
 
 // This has no meaning with out rtdebug also turned on.

src/libstd/str.rs
@@ -101,6 +101,7 @@ use ptr::RawPtr;
 use from_str::FromStr;
 use slice;
 use slice::{OwnedVector, OwnedCloneableVector, ImmutableVector, MutableVector};
+use slice::{Vector};
 use vec::Vec;
 use default::Default;
 use raw::Repr;