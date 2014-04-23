rust-lang/rust

Support unsized types with the `type` keyword

src/librustc/metadata/common.rs
@@ -169,6 +169,7 @@ pub static tag_lang_items_item_node_id: uint = 0x4b;
 
 pub static tag_item_unnamed_field: uint = 0x4c;
 pub static tag_items_data_item_visibility: uint = 0x4e;
+pub static tag_items_data_item_sized: uint = 0x4f;
 
 pub static tag_item_method_tps: uint = 0x51;
 pub static tag_item_method_fty: uint = 0x52;

moshg/rust-std-ja

src/librustc/metadata/common.rs
@@ -170,6 +170,7 @@ pub static tag_lang_items_item_node_id: uint = 0x73;
 
 pub static tag_item_unnamed_field: uint = 0x74;
 pub static tag_items_data_item_visibility: uint = 0x76;
+pub static tag_items_data_item_sized: uint = 0x77;
 
 pub static tag_item_method_tps: uint = 0x79;
 pub static tag_item_method_fty: uint = 0x7a;