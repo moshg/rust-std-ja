rust-lang/rust

testsuite: merge some lexer testcases
Now that the lexer is more robust, these tests don't need to be in separate
files. Yay!

src/test/compile-fail/lex-bad-char-literals.rs
@@ -31,5 +31,19 @@ static s: &'static str =
 
 static s2: &'static str =
     "\u23q" //~ ERROR: illegal character in numeric character escape
+    //~^ ERROR: numeric character escape is too short
 ;
-//~^^ ERROR: numeric character escape is too short
+
+static c: char =
+    '\●' //~ ERROR: unknown character escape
+;
+
+static s: &'static str =
+    "\●" //~ ERROR: unknown string escape
+;
+
+// THIS MUST BE LAST, since unterminated character constants kill the lexer
+
+static c: char =
+    '●  //~ ERROR: unterminated character constant
+;

moshg/rust-std-ja

src/test/compile-fail/lex-bad-char-literals.rs
@@ -31,5 +31,19 @@ static s: &'static str =
 
 static s2: &'static str =
     "\u23q" //~ ERROR: illegal character in numeric character escape
+    //~^ ERROR: numeric character escape is too short
 ;
-//~^^ ERROR: numeric character escape is too short
+
+static c: char =
+    '\●' //~ ERROR: unknown character escape
+;
+
+static s: &'static str =
+    "\●" //~ ERROR: unknown character escape
+;
+
+// THIS MUST BE LAST, since unterminated character constants kill the lexer
+
+static c: char =
+    '●  //~ ERROR: unterminated character constant
+;