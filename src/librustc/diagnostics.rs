rust-lang/rust

Produce a better error for irrefutable `if let` patterns
Modify ast::ExprMatch to include a new value of type ast::MatchSource,
making it easy to tell whether the match was written literally or
produced via desugaring. This allows us to customize error messages
appropriately.

src/librustc/diagnostics.rs
@@ -17,6 +17,11 @@ register_diagnostic!(E0001, r##"
     one is too specific or the ordering is incorrect.
 "##)
 
+register_diagnostic!(E0159, r##"
+    This error is produced by an `if let` expression where the pattern is irrefutable.
+    An `if let` that can never fail is considered an error.
+"##)
+
 register_diagnostics!(
     E0002,
     E0003,

moshg/rust-std-ja

src/librustc/diagnostics.rs
@@ -19,6 +19,11 @@ register_diagnostic!(E0001, r##"
     one is too specific or the ordering is incorrect.
 "##)
 
+register_diagnostic!(E0162, r##"
+    This error is produced by an `if let` expression where the pattern is irrefutable.
+    An `if let` that can never fail is considered an error.
+"##)
+
 register_diagnostics!(
     E0002,
     E0003,