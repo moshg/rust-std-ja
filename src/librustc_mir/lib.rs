alexreg/rust

Report const eval error inside the query

src/librustc_mir/lib.rs
@@ -94,6 +94,7 @@ pub fn provide(providers: &mut Providers) {
     shim::provide(providers);
     transform::provide(providers);
     providers.const_eval = interpret::const_eval_provider;
+    providers.const_eval_raw = interpret::const_eval_raw_provider;
     providers.check_match = hair::pattern::check_match;
 }
 

moshg/rust-std-ja

src/librustc_mir/lib.rs
@@ -94,6 +94,7 @@ pub fn provide(providers: &mut Providers) {
     shim::provide(providers);
     transform::provide(providers);
     providers.const_eval = const_eval::const_eval_provider;
+    providers.const_eval_raw = const_eval::const_eval_raw_provider;
     providers.check_match = hair::pattern::check_match;
 }
 